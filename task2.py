ef0d66cb5c28f8d4a74ed4fb3987c21794618aca
xingyushu <1402598162@qq.com>
2023-03-15 14:54:06 -0400

    Update task2.py

--- task2.py
+++ task2.py
@@ -1,24 +1,21 @@
 import torch
 import torch.nn as nn
 import torch.optim as optim
-import torchvision.transforms as transforms
 from torchvision.datasets import MNIST
 from torch.utils.data import DataLoader
 from torchvision import datasets, transforms
-import torch
-import torch.nn as nn
-import torch.optim as optim
 import torch.nn.functional as F
 import numpy as np
 import matplotlib.pyplot as plt
 # from __future__ import print_function
-from torchvision import datasets, transforms
 
 
 epsilons = [0, .05, .1, .15, .2, .25, .3, .35, .4, .45, .5]
 
 use_cuda = False
 
+
+print("-------------------------------------PREPARE THE MODEL----------------------------------------------------")
 
 # Define the model architecture
 class Net(nn.Module):
@@ -40,11 +37,16 @@ class Net(nn.Module):
         return nn.functional.log_softmax(x, dim=1)
 
 # Set up the training and test data loaders
+
 transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))])
-train_data = MNIST('data', train=True, download=True, transform=transform)
-train_loader = DataLoader(train_data, batch_size=64, shuffle=True)
-test_data = MNIST('data', train=False, download=True, transform=transform)
-test_loader = DataLoader(test_data, batch_size=1000, shuffle=False)
+
+train_data = datasets.MNIST('data2/', train=True, download=True, transform=transform)
+train_loader = torch.utils.data.DataLoader(train_data, batch_size=64, shuffle=True)
+
+test_data = datasets.MNIST('data2/', train=False, download=True, transform=transform)
+test_loader = torch.utils.data.DataLoader(test_data, batch_size=1000, shuffle=False)
+
+
 
 # Initialize the model, loss function, and optimizer
 model = Net()
@@ -53,6 +55,7 @@ optimizer = optim.Adam(model.parameters(), lr=0.001)
 
 train_acc = []
 test_accs = []
+
 
 # Train the model for 10 epochs
 for epoch in range(200):
@@ -108,38 +111,17 @@ for epoch in range(200):
     test_accs.append(test_acc)
     print('Test set: Average loss: {:.4f}, Accuracy: {:.2f}%'.format(test_loss, test_acc))
 
-import matplotlib.pyplot as plt
 
 # Plot the training accuracy
 plt.plot(range(1, len(train_acc)+1), train_acc, 'b')
-plt.legend(['Training Accuracy'],loc='lower right')
-# plt.title('Training accuracy')
-plt.xlabel('Epoch')
-plt.ylabel('Accuracy (%)')
-# plt.xticks(range(1, len(train_acc)+1))
-plt.savefig('train_accuracy_task2_without_attack.pdf')
-# plt.show()
-
-
-# plt.plot(train_acc, label='Training Accuracy')
-plt.plot(test_accs,'r')
-plt.legend(['Training Accuracy','Testing Accuracy'],loc='lower right')
+plt.plot(range(1, len(test_accs)+1),test_accs,'r')
+plt.legend(['Training Accuracy','Testing Accuracy'],loc='lower right', fontsize=8)
 # plt.title('Testing accuracy')
 plt.xlabel('Epoch')
 plt.ylabel('Accuracy(%)')
 # plt.legend()
 plt.savefig('test_accuracy_task2_without_attack.pdf')
-# plt.show()
-
-
-# Save the training and testing accuracy to a file
-# with open('accuracy.txt', 'w') as f:
-#     f.write('Epoch\tTraining Accuracy\tTest Accuracy\n')
-#     for i in range(len(train_acc)):
-#         f.write('{}\t{}\t{}\n'.format(i, train_acc[i], test_accs[i]))
-
-# Plot the training and testing accuracy
-import matplotlib.pyplot as plt
+plt.show()
 
 
 # Evaluate the success rate of the FGSM attack for each epsilon value
@@ -179,26 +161,6 @@ plt.savefig('success_rate_task2.pdf')
 # plt.show()
 
 
-# Plot the training accuracy
-# plt.plot(range(1, len(train_acc)+1), train_acc, 'b')
-# plt.legend(['Training Accuracy'],loc='lower right')
-# # plt.title('Training accuracy')
-# plt.xlabel('Epoch')
-# plt.ylabel('Accuracy (%)')
-# # plt.xticks(range(1, len(train_acc)+1))
-# plt.savefig('train_accuracy_task2.pdf')
-# # plt.show()
-
-
-# # plt.plot(train_acc, label='Training Accuracy')
-# plt.plot(test_accs,'r')
-# plt.legend(['Training Accuracy','Testing Accuracy'],loc='lower right')
-# # plt.title('Testing accuracy')
-# plt.xlabel('Epoch')
-# plt.ylabel('Accuracy(%)')
-# # plt.legend()
-# plt.savefig('test_accuracy_task2.pdf')
-# # plt.show()
 
 
 print("-------------------------------------Begin ATTACK----------------------------------------------------")
@@ -208,7 +170,7 @@ print("-------------------------------------Begin ATTACK------------------------
 
 
 
-# download the dataset
+# load the dataset
 loader = torch.utils.data.DataLoader(
     datasets.MNIST('data2/', train=False, download=True, transform=transforms.Compose([
         transforms.ToTensor(),
@@ -218,13 +180,6 @@ loader = torch.utils.data.DataLoader(
 )
 
 
-# Initialize the model, loss function, and optimizer
-model = Net()
-criterion = nn.NLLLoss()
-optimizer = optim.Adam(model.parameters(), lr=0.001)
-
-train_acc = []
-test_accs = []
 
 
 # Load the victim model
@@ -283,7 +238,7 @@ def test( model, device, test_loader, epsilon ):
         #launch the fgsm_attack_module
         perturbed_data = fgsm_attack_module(data, epsilon, data_grad)
 
-        # run the model on perturbed_data 
+        # run the model on perturbed_data
         output = model(perturbed_data)
 
         final_pred = output.max(1, keepdim=True)[1] # get the index of the max log-probability
@@ -327,7 +282,7 @@ plt.savefig('test_accuracy_task2_with_Epsilon.pdf')
 
 # Plot several examples of adversarial samples at each epsilon
 cnt = 0
-plt.figure(figsize=(8,10))
+plt.figure(figsize=(8,8))
 for i in range(len(epsilons)):
     for j in range(len(examples[i])):
         cnt += 1
@@ -335,11 +290,11 @@ for i in range(len(epsilons)):
         plt.xticks([], [])
         plt.yticks([], [])
         if j == 0:
-            plt.ylabel("Eps: {}".format(epsilons[i]), fontsize=14)
+            plt.ylabel("Eps: {}".format(epsilons[i]), fontsize=9)
         orig,adv,ex = examples[i][j]
         plt.title("{} -> {}".format(orig, adv))
-        plt.savefig('examples_task2.pdf')
         plt.imshow(ex, cmap="gray")
 plt.tight_layout()
+plt.savefig('examples_task2.pdf')
 plt.show()
 
