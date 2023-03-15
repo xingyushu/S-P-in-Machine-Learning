1d15ba89b8dc8fea9797da91e75ca788ba789164
xingyushu <1402598162@qq.com>
2023-03-14 21:57:16 -0400

    Update task2.py

--- task2.py
+++ task2.py
@@ -4,8 +4,21 @@ import torch.optim as optim
 import torchvision.transforms as transforms
 from torchvision.datasets import MNIST
 from torch.utils.data import DataLoader
+from torchvision import datasets, transforms
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
 import numpy as np
 import matplotlib.pyplot as plt
+# from __future__ import print_function
+from torchvision import datasets, transforms
+
+
+epsilons = [0, .05, .1, .15, .2, .25, .3, .35, .4, .45, .5]
+
+use_cuda = False
+
 
 # Define the model architecture
 class Net(nn.Module):
@@ -41,10 +54,8 @@ optimizer = optim.Adam(model.parameters(), lr=0.001)
 train_acc = []
 test_accs = []
 
-
 # Train the model for 10 epochs
-'''
-for epoch in range(10):
+for epoch in range(200):
     model.train()
     for batch_idx, (data, target) in enumerate(train_loader):
         optimizer.zero_grad()
@@ -53,10 +64,33 @@ for epoch in range(10):
         loss.backward()
         optimizer.step()
 
+
+        # compute training accuracy and print it on the shell
+        _, predicted = torch.max(output.data, 1)
+        correct = (predicted == target).sum().item()
+        total = target.size(0)
+        accuracy = 100. * correct / total
+        # train_acc.append(accuracy)
+        
+
         if batch_idx % 100 == 0:
-            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'
+            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}\tAccuracy: {:.2f}%'
                   .format(epoch, batch_idx * len(data), len(train_loader.dataset),
-                          100. * batch_idx / len(train_loader), loss.item()))
+                          100. * batch_idx / len(train_loader), loss.item(), accuracy))
+        
+        
+        if (batch_idx + 1) == len(train_loader):
+            # Compute training accuracy only after last batch of the epoch
+            _, predicted = torch.max(output.data, 1)
+            correct = (predicted == target).sum().item()
+            total = target.size(0)
+            accuracy = 100. * correct / total
+            train_acc.append(accuracy)
+
+    # Save the model after each epoch
+    torch.save(model.state_dict(), 'mnist_model.pt')
+
+
 
     # Evaluate the model on the test data
     model.eval()
@@ -74,95 +108,238 @@ for epoch in range(10):
     test_accs.append(test_acc)
     print('Test set: Average loss: {:.4f}, Accuracy: {:.2f}%'.format(test_loss, test_acc))
 
+import matplotlib.pyplot as plt
 
-# Save the trained model
-torch.save(model.state_dict(), 'mnist_cnn.pt')
-'''
-
-# FGSM AE Attack
-epsilon = 0.1  # perturbation strength
-alpha = 0.05   # step size for attack iterations
-steps = 10     # number of attack iterations
-
-# define loss function for the attack
-# define the FGSM AE loss function
-def fgsm_ae_loss(output, target, perturbed_data, criterion, epsilon):
-    # calculate the loss
-    # first, calculate the original loss between the output and target
-    original_loss = criterion(output, target)
-
-    # calculate the perturbation as the sign of the gradient of the loss w.r.t the input
-    perturbation = torch.sign(torch.autograd.grad(original_loss, perturbed_data, 
-                                                   retain_graph=False, create_graph=False)[0])
-
-    # perturb the data by adding the perturbation scaled by epsilon
-    perturbed_data = perturbed_data + epsilon * perturbation
-
-    # clamp the perturbed data between 0 and 1
-    perturbed_data = torch.clamp(perturbed_data, 0, 1)
-
-    # calculate the loss between the perturbed data and the original input data
-    ae_loss = criterion(perturbed_data, data)
-
-    # combine the losses and return
-    total_loss = original_loss + ae_loss
-    return total_loss
-
-# load the MNIST test set
-
-# test_data = MNIST('data', train=False, download=True, transform=transform)
-# test_loader = DataLoader(test_data, batch_size=1000, shuffle=False)
+# Plot the training accuracy
+plt.plot(range(1, len(train_acc)+1), train_acc, 'b')
+plt.legend(['Training Accuracy'],loc='lower right')
+# plt.title('Training accuracy')
+plt.xlabel('Epoch')
+plt.ylabel('Accuracy (%)')
+# plt.xticks(range(1, len(train_acc)+1))
+plt.savefig('train_accuracy_task2_without_attack.pdf')
+# plt.show()
 
 
-# test_dataset =MNIST(root='data', train=False, download=True, transform=transforms.ToTensor())
-# test_loader = DataLoader(test_dataset, batch_size=1, shuffle=True)
+# plt.plot(train_acc, label='Training Accuracy')
+plt.plot(test_accs,'r')
+plt.legend(['Training Accuracy','Testing Accuracy'],loc='lower right')
+# plt.title('Testing accuracy')
+plt.xlabel('Epoch')
+plt.ylabel('Accuracy(%)')
+# plt.legend()
+plt.savefig('test_accuracy_task2_without_attack.pdf')
+# plt.show()
 
-# load the pre-trained model
-model = torch.load('mnist_cnn.pt')
 
-# set the device
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-# model.to(device)
+# Save the training and testing accuracy to a file
+# with open('accuracy.txt', 'w') as f:
+#     f.write('Epoch\tTraining Accuracy\tTest Accuracy\n')
+#     for i in range(len(train_acc)):
+#         f.write('{}\t{}\t{}\n'.format(i, train_acc[i], test_accs[i]))
 
-# define the criterion
-# criterion = nn.CrossEntropyLoss()
+# Plot the training and testing accuracy
+import matplotlib.pyplot as plt
 
-# set the epsilon for the FGSM AE attack
-epsilon = 0.1
 
-# perform the FGSM AE attack on a sample image from the test set
-# model.eval()
-data, target = next(iter(test_loader))
-data = data.to(device)
-target = target.to(device)
-data.requires_grad = True
-output = model(data)
-loss = criterion(output, target)
-loss.backward()
-perturbed_data = data + epsilon * torch.sign(data.grad)
-perturbed_data = torch.clamp(perturbed_data, 0, 1)
+# Evaluate the success rate of the FGSM attack for each epsilon value
+success_rates = []
+for epsilon in epsilons:
+    num_correct = 0
+    num_total = 0
+    for images, labels in test_loader:
+        # Generate adversarial examples using FGSM attack
+        images.requires_grad = True
+        output = model(images)
+        loss = nn.functional.nll_loss(output, labels)
+        loss.backward()
 
-# evaluate the model on the perturbed data
-# model.eval()
-output = model(perturbed_data)
-test_loss = criterion(output, target).item()
-pred = output.argmax(dim=1, keepdim=True)
-correct = pred.eq(target.view_as(pred)).sum().item()
+        # Compute the sign of the gradient with respect to the input data
+        images_grad = images.grad.data.sign()
 
-# calculate the success rate of the victim model
-success_rate = 100. * (1 - correct / len(target))
+        # Perturb the input data with the sign of the gradient to generate adversarial examples
+        perturbed_images = images + epsilon * images_grad
+        perturbed_images = torch.clamp(perturbed_images, 0, 1)
 
-# print the results
-print('Original image:')
-plt.imshow(data[0][0].cpu(), cmap='gray')
+        # Evaluate the victim model on the adversarial examples
+        output = model(perturbed_images)
+        _, predicted = torch.max(output.data, 1)
+        num_correct += (predicted == labels).sum().item()
+        num_total += labels.size(0)
+
+    success_rate = num_correct / num_total
+    success_rates.append(success_rate)
+    print('Epsilon: {}\tSuccess rate: {:.2f}%'.format(epsilon, 100. * success_rate))
+
+# Plot the success rate as a function of epsilon
+plt.plot(epsilons, success_rates)
+plt.xlabel('Epsilon')
+plt.ylabel('Success rate (%)')
+plt.savefig('success_rate_task2.pdf')
+# plt.show()
+
+
+# Plot the training accuracy
+# plt.plot(range(1, len(train_acc)+1), train_acc, 'b')
+# plt.legend(['Training Accuracy'],loc='lower right')
+# # plt.title('Training accuracy')
+# plt.xlabel('Epoch')
+# plt.ylabel('Accuracy (%)')
+# # plt.xticks(range(1, len(train_acc)+1))
+# plt.savefig('train_accuracy_task2.pdf')
+# # plt.show()
+
+
+# # plt.plot(train_acc, label='Training Accuracy')
+# plt.plot(test_accs,'r')
+# plt.legend(['Training Accuracy','Testing Accuracy'],loc='lower right')
+# # plt.title('Testing accuracy')
+# plt.xlabel('Epoch')
+# plt.ylabel('Accuracy(%)')
+# # plt.legend()
+# plt.savefig('test_accuracy_task2.pdf')
+# # plt.show()
+
+
+print("-------------------------------------Begin ATTACK----------------------------------------------------")
+
+
+
+
+
+
+# download the dataset
+loader = torch.utils.data.DataLoader(
+    datasets.MNIST('data2/', train=False, download=True, transform=transforms.Compose([
+        transforms.ToTensor(),
+    ])),
+    batch_size=1,
+    shuffle=True
+)
+
+
+# Initialize the model, loss function, and optimizer
+model = Net()
+criterion = nn.NLLLoss()
+optimizer = optim.Adam(model.parameters(), lr=0.001)
+
+train_acc = []
+test_accs = []
+
+
+# Load the victim model
+model = Net()
+model.load_state_dict(torch.load('mnist_model.pt'))
+model.eval()
+
+
+cuda_available = torch.cuda.is_available()
+device = torch.device('cude' if (use_cuda and cuda_available) else 'cpu')
+print('CUDA is available: ', cuda_available)
+
+
+# FGSM Attack Module
+def fgsm_attack_module(image, epsilon, data_grad):
+ 
+    sign_data_grad = data_grad.sign()
+    adversarial_image = image + epsilon * sign_data_grad
+    # set range to [0,1]
+    adversarial_image = torch.clamp(adversarial_image, 0, 1)
+    return adversarial_image
+
+
+
+def test( model, device, test_loader, epsilon ):
+
+    correct = 0
+    # adverserial examples
+    adv_examples = []
+
+    for data, target in test_loader:
+
+        # send the data and labels to device
+        data, target = data.to(device), target.to(device)
+
+        # set  requires_grad in tensor
+        data.requires_grad = True
+
+        # forward data 
+        output = model(data)
+        init_pred = output.max(1, keepdim=True)[1] # get the index of the max log-probability
+
+        # check the init  predict == target ? If not,continue
+        if init_pred.item() != target.item():
+            continue
+
+        loss = F.nll_loss(output, target)
+
+        model.zero_grad()
+
+        loss.backward()
+
+        # collect datagrad
+        data_grad = data.grad.data
+
+        #launch the fgsm_attack_module
+        perturbed_data = fgsm_attack_module(data, epsilon, data_grad)
+
+        # run the model on perturbed_data
+        output = model(perturbed_data)
+
+        final_pred = output.max(1, keepdim=True)[1] # get the index of the max log-probability
+        if final_pred.item() == target.item():
+            correct += 1
+            if (epsilon == 0) and (len(adv_examples) < 5):
+                adv_ex = perturbed_data.squeeze().detach().cpu().numpy()
+                adv_examples.append( (init_pred.item(), final_pred.item(), adv_ex) )
+        else:
+            if len(adv_examples) < 5:
+                adv_ex = perturbed_data.squeeze().detach().cpu().numpy()
+                adv_examples.append( (init_pred.item(), final_pred.item(), adv_ex) )
+
+    final_acc = correct / float(len(test_loader))
+    print("Epsilon: {}\tTest Accuracy = {} / {} = {}".format(epsilon, correct, len(test_loader), final_acc))
+
+
+    return final_acc, adv_examples
+
+
+
+accuracies = []
+examples = []
+
+# Run test for each epsilon
+for eps in epsilons:
+    acc, ex = test(model, device,loader, eps)
+    accuracies.append(acc)
+    examples.append(ex)
+
+plt.figure(figsize=(5,5))
+plt.plot(epsilons, accuracies, "*-")
+plt.yticks(np.arange(0, 1.1, step=0.1))
+plt.xticks(np.arange(0, .35, step=0.05))
+plt.title("Accuracy vs Epsilon")
+plt.xlabel("Epsilon")
+plt.ylabel("Accuracy")
+plt.savefig('test_accuracy_task2_with_Epsilon.pdf')
+# plt.show()
+
+
+# Plot several examples of adversarial samples at each epsilon
+cnt = 0
+plt.figure(figsize=(8,10))
+for i in range(len(epsilons)):
+    for j in range(len(examples[i])):
+        cnt += 1
+        plt.subplot(len(epsilons),len(examples[0]),cnt)
+        plt.xticks([], [])
+        plt.yticks([], [])
+        if j == 0:
+            plt.ylabel("Eps: {}".format(epsilons[i]), fontsize=14)
+        orig,adv,ex = examples[i][j]
+        plt.title("{} -> {}".format(orig, adv))
+        plt.savefig('examples_task2.pdf')
+        plt.imshow(ex, cmap="gray")
+plt.tight_layout()
 plt.show()
 
-print('Perturbed image:')
-plt.imshow(perturbed_data[0][0].cpu(), cmap='gray')
-plt.show()
-
-print('Prediction on original image:', output.argmax(dim=1)[0].item())
-print('Prediction on perturbed image:', pred[0].item())
-print('Test loss on perturbed image:', test_loss)
-print('Test accuracy on perturbed image: {:.2f}%'.format(100. * correct / len(target)))
-print('Success rate of the victim model: {:.2f}%'.format(success_rate))
